86ba6cad92e97462de720a78462f0f23bed750ef
Pavel <palllkaignatev@gmail.com>
2025-07-05 20:26:24 +0300

    Release

--- src/modules/crons/crons.js
+++ src/modules/crons/crons.js
@@ -16,11 +16,14 @@ function numberInArray(num, arr) {
 
 const crons = () => {
   // при инициализации сервера чтобы isUpdated переходило в false
-  writeFile("src/store/update/update.json", JSON.stringify({ isUpdated: false }));
+  writeFile(
+    "src/store/update/update.json",
+    JSON.stringify({ isUpdated: false })
+  );
 
   const updateServerCron = new CronJob("0 0 * * *", function () {
     if (numberInArray(global.app.server.address().port)) {
-      // updateServer();
+      updateServer();
     }
   });
 
