367e69fc9ed877fed5efbd5db7eb6036e145b3ed
Pavel <palllkaignatev@gmail.com>
2025-08-27 17:09:17 +0600

    Release

--- client/src/components/ProcessingIndicator/ProcessingIndicator.tsx
+++ client/src/components/ProcessingIndicator/ProcessingIndicator.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useStore } from "effector-react";
-import { $isProcessing, $processedCount, $totalCount } from "../../store/Table/state";
+import { $isProcessing, $processedCount, $totalCount, $currentStage, $totalStages } from "../../store/Table/state";
 import classes from "./ProcessingIndicator.module.scss";
 
 export const ProcessingIndicator = () => {
   const isProcessing = useStore($isProcessing);
   const processedCount = useStore($processedCount);
   const totalCount = useStore($totalCount);
+  const currentStage = useStore($currentStage);
+  const totalStages = useStore($totalStages);
 
   if (!isProcessing) {
     return null;
@@ -17,7 +19,7 @@ export const ProcessingIndicator = () => {
   return (
     <div className={classes.container}>
       <div className={classes.content}>
-        <div className={classes.title}>Tournament processing...</div>
+        <div className={classes.title}>Tournament processing... {currentStage}/{totalStages}</div>
         <div className={classes.progressBar}>
           <div 
             className={classes.progressFill} 
