9f9262abf77b31a49a63d883ed9638a5f2c92eeb
Pavel <palllkaignatev@gmail.com>
2025-08-27 11:33:24 +0600

    Release

--- client/src/store/Filter/init.ts
+++ client/src/store/Filter/init.ts
@@ -4,51 +4,95 @@ import { $filterContent } from "./state";
 
 import api from "../../api";
 
+const isFunctionSupported = () => {
+  try {
+    new Function('return true')();
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const checkMobileSupport = () => {
+  const functionSupported = isFunctionSupported();
+  
+  if (!functionSupported) {
+    alert('Search functionality is not supported in your browser. Please try another browser (Chrome, Firefox, Safari, Edge) or update your browser.');
+  }
+  
+  return { functionSupported };
+};
+
+const parseModuleSafely = (code: string, exportName: string) => {
+  const support = checkMobileSupport();
+
+  if (support.functionSupported) {
+    try {
+      if (exportName === 'filter') {
+        // @ts-ignore
+        const { filter } = new Function(
+          // @ts-ignore
+          code.replace(
+            // @ts-ignore
+            "module.exports = filter_1;",
+            // @ts-ignore
+            "return { filter: filter_1};"
+            // @ts-ignore
+          ).replace(
+            // @ts-ignore
+            "module.exports = filter$1;",
+            // @ts-ignore
+            "return { filter: filter_1};"
+            // @ts-ignore
+          )
+          // @ts-ignore
+        )();
+        return filter;
+      } else if (exportName === 'scores') {
+        // @ts-ignore
+        const { scores } = new Function(
+          // @ts-ignore
+          code.replace(
+            // @ts-ignore
+            "module.exports = scores_1;",
+            // @ts-ignore
+            "return { scores: scores_1 };"
+            // @ts-ignore
+          ).replace(
+            // @ts-ignore
+            "module.exports = scores$1;",
+            // @ts-ignore
+            "return { scores: scores_1 };"
+            // @ts-ignore
+          )
+          // @ts-ignore
+        )();
+        return scores;
+      }
+    } catch (error) {
+      console.warn(`new Function failed for ${exportName}:`, error);
+    }
+  }
+  
+  return null;
+};
+
 export const fetchFilterContent = createEffect(async () => {
   const { filter: frontFilter, scores: frontScores }: any = await api.get("api/filter");
 
   try {
-    // @ts-ignore
-    const { filter } = new Function(
-      // @ts-ignore
-      frontFilter.replace(
-        // @ts-ignore
-        "module.exports = filter_1;",
-        // @ts-ignore
-        "return { filter: filter_1};"
-        // @ts-ignore
-      ).replace(
-        // @ts-ignore
-        "module.exports = filter$1;",
-        // @ts-ignore
-        "return { filter: filter_1};"
-        // @ts-ignore
-      )
-      // @ts-ignore
-    )();
-
-    // @ts-ignore
-    const { scores } = new Function(
-      // @ts-ignore
-      frontScores.replace(
-        // @ts-ignore
-        "module.exports = scores_1;",
-        // @ts-ignore
-        "return { scores: scores_1 };"
-        // @ts-ignore
-      ).replace(
-        // @ts-ignore
-        "module.exports = scores$1;",
-        // @ts-ignore
-        "return { scores: scores_1 };"
-        // @ts-ignore
-      )
-      // @ts-ignore
-    )();
+    const filter = parseModuleSafely(frontFilter, 'filter');
+    const scores = parseModuleSafely(frontScores, 'scores');
+    
+    if (!filter || !scores) {
+      alert('Search functionality is not working in your browser. Please try another browser.');
+      return { filter: [], scores: [] };
+    }
         
-    return {filter: filter.filter, scores: scores.scores};
+    return { filter: filter.filter, scores: scores.scores };
   } catch (error) {
-    console.log(error);
+    alert('Error loading search functionality. Please try another browser.');
+    return { filter: [], scores: [] };
   }
 });
 
