adaef00b0f2ffa52a48e8da6e0bc7a5615b67ae0
Pavel <palllkaignatev@gmail.com>
2025-08-29 17:32:53 +0600

    Release

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,9 +8,24 @@ import { AccessDeniedPage } from "./AccessDeniedPage";
 
 export const Pages: FC = () => {
   useEffect(() => {
-    const ua = navigator.userAgent || navigator.vendor || "";
-    const isFirefox = /Firefox|FxiOS/i.test(ua);
-    const isOpera = /OPR|Opera|OPiOS/i.test(ua);
+    const ua = navigator.userAgent || "";
+    const vendor = navigator.vendor || "";
+    const appName = navigator.appName || "";
+    const product = navigator.product || "";
+    const appVersion = navigator.appVersion || "";
+    
+    const isFirefox = /Firefox|FxiOS|Firefox\/\d+|Mozilla.*Firefox|Iceweasel|Seamonkey|Waterfox|Pale Moon|Basilisk|LibreWolf|Floorp|Borealis|Mozilla\/5\.0.*Firefox|Mozilla\/5\.0.*Gecko.*Firefox|Mozilla\/5\.0.*rv:\d+\.\d+.*Gecko.*Firefox/i.test(ua) ||
+                     /Firefox|FxiOS|Firefox\/\d+|Mozilla.*Firefox|Iceweasel|Seamonkey|Waterfox|Pale Moon|Basilisk|LibreWolf|Floorp|Borealis|Mozilla\/5\.0.*Firefox|Mozilla\/5\.0.*Gecko.*Firefox|Mozilla\/5\.0.*rv:\d+\.\d+.*Gecko.*Firefox/i.test(vendor) ||
+                     /Firefox|FxiOS|Firefox\/\d+|Mozilla.*Firefox|Iceweasel|Seamonkey|Waterfox|Pale Moon|Basilisk|LibreWolf|Floorp|Borealis|Mozilla\/5\.0.*Firefox|Mozilla\/5\.0.*Gecko.*Firefox|Mozilla\/5\.0.*rv:\d+\.\d+.*Gecko.*Firefox/i.test(appName) ||
+                     /Firefox|FxiOS|Firefox\/\d+|Mozilla.*Firefox|Iceweasel|Seamonkey|Waterfox|Pale Moon|Basilisk|LibreWolf|Floorp|Borealis|Mozilla\/5\.0.*Firefox|Mozilla\/5\.0.*Gecko.*Firefox|Mozilla\/5\.0.*rv:\d+\.\d+.*Gecko.*Firefox/i.test(product) ||
+                     /Firefox|FxiOS|Firefox\/\d+|Mozilla.*Firefox|Iceweasel|Seamonkey|Waterfox|Pale Moon|Basilisk|LibreWolf|Floorp|Borealis|Mozilla\/5\.0.*Firefox|Mozilla\/5\.0.*Gecko.*Firefox|Mozilla\/5\.0.*rv:\d+\.\d+.*Gecko.*Firefox/i.test(appVersion);
+    
+    const isOpera = /OPR|Opera|OPiOS|Opera\/\d+|Mozilla.*Opera|Opera Mini|Opera Mobile|Opera Next|Opera Touch|Opera GX|Mozilla\/5\.0.*Opera|Mozilla\/5\.0.*Presto.*Opera|Mozilla\/5\.0.*Chrome.*Safari.*OPR|Mozilla\/5\.0.*Chrome.*Safari.*Opera/i.test(ua) ||
+                   /OPR|Opera|OPiOS|Opera\/\d+|Mozilla.*Opera|Opera Mini|Opera Mobile|Opera Next|Opera Touch|Opera GX|Mozilla\/5\.0.*Opera|Mozilla\/5\.0.*Presto.*Opera|Mozilla\/5\.0.*Chrome.*Safari.*OPR|Mozilla\/5\.0.*Chrome.*Safari.*Opera/i.test(vendor) ||
+                   /OPR|Opera|OPiOS|Opera\/\d+|Mozilla.*Opera|Opera Mini|Opera Mobile|Opera Next|Opera Touch|Opera GX|Mozilla\/5\.0.*Opera|Mozilla\/5\.0.*Presto.*Opera|Mozilla\/5\.0.*Chrome.*Safari.*OPR|Mozilla\/5\.0.*Chrome.*Safari.*Opera/i.test(appName) ||
+                   /OPR|Opera|OPiOS|Opera\/\d+|Mozilla.*Opera|Opera Mini|Opera Mobile|Opera Next|Opera Touch|Opera GX|Mozilla\/5\.0.*Opera|Mozilla\/5\.0.*Presto.*Opera|Mozilla\/5\.0.*Chrome.*Safari.*OPR|Mozilla\/5\.0.*Chrome.*Safari.*Opera/i.test(product) ||
+                   /OPR|Opera|OPiOS|Opera\/\d+|Mozilla.*Firefox|Opera Mini|Opera Mobile|Opera Next|Opera Touch|Opera GX|Mozilla\/5\.0.*Opera|Mozilla\/5\.0.*Presto.*Opera|Mozilla\/5\.0.*Chrome.*Safari.*OPR|Mozilla\/5\.0.*Chrome.*Safari.*Opera/i.test(appVersion);
+    
     const isSupported = isFirefox || isOpera;
     if (!isSupported && window.location.pathname !== "/access-denied" && !window.location.pathname.startsWith("/admin")) {
       window.location.replace("/access-denied");
